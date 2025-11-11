7a7eaa132083e284d2b06bbd417a696ea5829d5e
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-11-11 08:08:11 +0600

    cmnt-upd

--- src/myComponents/HomePage/HomePageHero.tsx
+++ src/myComponents/HomePage/HomePageHero.tsx
@@ -48,6 +48,7 @@ export default function HomePageHero() {
     const [isOpenArtist, setIsOpenArtist] = useState<boolean>(false);
     const [searchArtist, setSearchArtist] = useState<string>('');
     const [selectedArtist, setSelectedArtist] = useState<Artist | null>(null);
+
     // Added type to userefArtist
     const refArtist = useRef<HTMLDivElement>(null);
 
@@ -72,8 +73,8 @@ export default function HomePageHero() {
     const [isOpenLocation, setIsOpenLocation] = useState<boolean>(false);
     const [searchLocation, setSearchLocation] = useState<string>('');
     const [selectedLocation, setSelectedLocation] = useState<Artist | null>(null);
-    // Added type to userefArtist
 
+    // Added type to userefArtist
     const refLocation = useRef<HTMLDivElement>(null);
 
     const filteredLocations = locations.filter(a =>
@@ -96,8 +97,8 @@ export default function HomePageHero() {
         <div className="mt-8 flex flex-col xl:flex-row gap-12">
             <div className="w-full xl:w-1/2">
                 <h1 className="font-medium font-IBM-plex-sans text-2xl lg:text-5xl">Smarter Talent Booking, All in One Place</h1>
-                <p className="text-[#6B7280] mt-6 text-xl">The all-in-one platform for agents, artists, venues, and buyers to searchArtist,
-                    book and grow - smarter and faster.</p>
+                <p className="text-[#6B7280] mt-6 text-xl">The all-in-one platform for agents, artists, venues, and buyers
+                    to searchArtist, book and grow - smarter and faster.</p>
                 <button className="bg-[#235789] text-[#FFFFFF] font-inter px-6 py-2 rounded-4xl flex gap-2 mt-6 cursor-pointer">
                     Join Now
                     <FaArrowRightLong className="translate-y-1/3" />
@@ -159,13 +160,13 @@ export default function HomePageHero() {
                             </div>
                         )}
                     </div>
+
                     {/* {selectedArtist && (
                         <div className="mt-8 p-6 bg-white rounded-2xl shadow-md">
                             <h2 className="text-lg font-semibold mb-2">selectedArtist Artist</h2>
                             <p className="text-xl font-bold">{selectedArtist.name}</p>
                         </div>
                     )} */}
-
 
                     <div className="w-full flex flex-col lg:flex-row gap-6 mt-4">
                         {/* Select Location */}
